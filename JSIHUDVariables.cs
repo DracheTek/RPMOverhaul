b0c8e40ea5e2da08ba3028af98c6e2547f7ae637
DracheTek <358396044@qq.com>
2015-04-18 01:35:38 -0500

    Update JSIHUDVariables.cs

--- JSIHUDVariables.cs
+++ JSIHUDVariables.cs
@@ -8,7 +8,6 @@ namespace JSI
 {
     public class InfoGauge : InternalModule
     {
-        public readonly int gaugeID;
         [KSPField] // 贴图
         public string Texture = string.Empty;
         [KSPField] // 绘图区的位置（从左上角开始），绘图区的大小（横，纵）
@@ -30,42 +29,41 @@ namespace JSI
         [KSPField] //随载具滚转
         public bool RotateWithVessel = false;
         public Material Material;
-        public static List<InfoGauge> gaugeList;
 
         //与信息条相关的变量
 
-        [KSPField]
-        public string cameraTransform = string.Empty;
-        private FlyingCamera cameraObject;
-        [KSPField]
-        public string backgroundColor = "0,0,0,0";
-        private Color32 backgroundColorValue;
-
-
-        private RasterPropMonitorComputer comp;//计算机系统实例
-        private bool startupComplete;//是否启动完成的变量
-
-
         public InfoGauge()
         {
-            InfoGauge gauge = null;
-            ConfigNode[] nodes = GameDatabase.Instance.GetConfigNodes("GAUGE");
-            foreach (ConfigNode gaugeNode in nodes)
-            {
-                gauge.Texture = gaugeNode.GetValue("Texture");
-                gauge.Position = ConfigNode.ParseVector4(gaugeNode.GetValue("Position"));
-                gauge.Limit = ConfigNode.ParseVector2(gaugeNode.GetValue("Limit"));
-                gauge.TextureLimit = ConfigNode.ParseVector2(gaugeNode.GetValue("TextureLimit"));
-                gauge.TextureSize = float.Parse(gaugeNode.GetValue("TextureSize"));
-                gauge.Variable = gaugeNode.GetValue("Variable");
-                gauge.VerticalMovement = bool.Parse(gaugeNode.GetValue("VerticalMovement"));
-                gauge.UseLog10 = bool.Parse(gaugeNode.GetValue("UseLog10"));
-                gauge.Use360Horizon = bool.Parse(gaugeNode.GetValue("Use360horizon"));
-                gauge.RotateWithVessel = bool.Parse(gaugeNode.GetValue("RotateWithVessel"));
-                gaugeList.Add(gauge);
-                JUtil.LogMessage(this, "Loading texture from URL, \"{0}\"", gaugeNode.GetValue("Texture"));
-                JUtil.LogMessage(this, "position \"{0}", gaugeNode.GetValue("Position"));
-            }
+            Texture = string.Empty;
+            Position = new Vector4(0f, 0f, 64f, 320f);
+            Limit = new Vector2(0f, 10000f);
+            TextureLimit = new Vector4(0.0f, 1.0f, 0.0f, 1.0f);
+            TextureSize = 0.5f;
+            Variable = string.Empty;
+            VerticalMovement = true;
+            UseLog10 = true;
+            Use360Horizon = true;
+            RotateWithVessel = false;
+            Material = null;
         }
+
+        //构造函数
+        public static InfoGauge ReadNode (ConfigNode node)
+        {
+            InfoGauge gauge = new InfoGauge();
+                gauge.Texture = node.GetValue("Texture");
+                gauge.Position = ConfigNode.ParseVector4(node.GetValue("Position"));
+                gauge.Limit = ConfigNode.ParseVector2(node.GetValue("Limit"));
+                gauge.TextureLimit = ConfigNode.ParseVector2(node.GetValue("TextureLimit"));
+                gauge.TextureSize = float.Parse(node.GetValue("TextureSize"));
+                gauge.Variable = node.GetValue("Variable");
+                gauge.VerticalMovement = bool.Parse(node.GetValue("VerticalMovement"));
+                gauge.UseLog10 = bool.Parse(node.GetValue("UseLog10"));
+                gauge.Use360Horizon = bool.Parse(node.GetValue("Use360horizon"));
+                gauge.RotateWithVessel = bool.Parse(node.GetValue("RotateWithVessel"));
+                return gauge;
+             
+        }//这个函数从没用过 The Method is never used.
     }
 }
+
